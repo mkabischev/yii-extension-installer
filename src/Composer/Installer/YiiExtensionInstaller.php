efa33b4b4f1ba4fd2526267d9065c576611e1259
mkabischev <kabischev.michail@wikimart.ru>
2013-02-18 09:48:19 +0400

    update composer.json

--- src/Composer/Installer/YiiExtensionInstaller.php
+++ src/Composer/Installer/YiiExtensionInstaller.php
@@ -6,6 +6,8 @@ use Composer\Package\PackageInterface;
 
 class YiiExtensionInstaller extends LibraryInstaller
 {
+    const DEFAULT_PATH = 'protected/extensions';
+
     public function supports( $packageType )
     {
         return 'yii-extension' === $packageType;
@@ -13,7 +15,12 @@ class YiiExtensionInstaller extends LibraryInstaller
 
     public function getInstallPath( PackageInterface $package )
     {
-        return 'another_dir/' . $package->getPrettyName();
+        $installPath = self::DEFAULT_PATH;
+        $extra       = $this->composer->getPackage()->getExtra();
+        if ( array_key_exists( 'yii_extensions_path', $extra ) ) {
+            $installPath = $extra['yii_extensions_path'];
+        }
+        return $installPath . DIRECTORY_SEPARATOR . $package->getPrettyName();
     }
 
 }
