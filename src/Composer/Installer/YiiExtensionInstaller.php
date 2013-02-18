fae705b9916b88f8e2d35d0edbd493827da06758
mkabischev <kabischev.michail@wikimart.ru>
2013-02-18 09:10:01 +0400

    init commit

---
+++ src/Composer/Installer/YiiExtensionInstaller.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Composer\Installer;
+
+use Composer\Package\PackageInterface;
+
+class YiiExtensionInstaller extends LibraryInstaller
+{
+    public function supports( $packageType )
+    {
+        return 'yii-extension' === $packageType;
+    }
+
+    public function getInstallPath( PackageInterface $package )
+    {
+        return 'another_dir/' . $package->getTargetDir();
+    }
+
+}
